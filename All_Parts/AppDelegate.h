526255d9af1c7208d630e6dcad84d30420b41b60
RuanSTao <ruanstao@sina.com>
2014-02-18 16:40:56 +0800

    Initial Commit

---
+++ All_Parts/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  All_Parts
+//
+//  Created by RuanSTao on 14-2-18.
+//  Copyright (c) 2014年 RuanSTao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ All_PartsTests/All_PartsTests.m
@@ -0,0 +1,34 @@
+//
+//  All_PartsTests.m
+//  All_PartsTests
+//
+//  Created by RuanSTao on 14-2-18.
+//  Copyright (c) 2014年 RuanSTao. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface All_PartsTests : XCTestCase
+
+@end
+
+@implementation All_PartsTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
